8571baad488350cd0370b5925b951e4de84cdebe
IshratAiJournal <imadmnawaz@gmail.com>
2025-06-12 14:29:44 +0530

    Create Day_2_my_story.py

---
+++ Day_2_my_story.py
@@ -0,0 +1,9 @@
+character_name = "Shabe Noor"
+character_age = "19"
+is_female = False
+print ("There once was a girl named " + character_name + ",")
+print ("She was " + character_age + "  years old.")
+
+character_name = "Noor jahan"
+print ("She really liked the name " + character_name + ",")
+print ("but she didn't like being " + character_age + ",")
