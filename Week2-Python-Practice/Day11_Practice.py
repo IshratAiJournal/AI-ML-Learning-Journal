c775fccf75cd70a8ae44446e8c3e5bf6645d1e23
IshratAiJournal <imadmnawaz@gmail.com>
2025-06-21 19:23:45 +0530

    Create Day11_Practice.py

---
+++ Week2-Python-Practice/Day11_Practice.py
@@ -0,0 +1,28 @@
+# 🐍 Practiced Day 11 – 21 June 2025
+
+## ✅ What I Did Today
+- Completed watching and practicing FreeCodeCamp video for beginner
+- Practiced Python class inheritance
+- Created `Rabbit` class with `jump()` and `eat()` methods
+- Explored interpreter behavior and error stopping
+- Learned how `try-except` keeps the program running
+- Worked on real examples: student grading, vehicles, bunny behavior
+
+
+## ⚠️ Challenges I Faced
+- Managing time with kids while practicing
+- Forgetting to log some of the completed code properly
+- Slight confusion with class re-definitions (student class used twice with different roles)
+
+
+## 💡 What Motivated Me
+- Seeing how much I’ve learned in just 11 days!   
+- My own growing confidence — I’m writing full programs now!
+- Day by day everyday I am getting better better and better
+
+
+## 🎯 Goal for Tomorrow (Day 12)
+- Practice file handling (read/write/append)
+- Learn about `os` module or `random` module
+- Try using functions inside classes (like methods calling other methods)
+- Stay consistent and practice even if it’s a busy day 💪
